51effa5614a531f7abc3a9670f9af666d77b2dc9
Mark <mrutenberg@users.noreply.github.com>
2016-06-28 14:12:04 +0100

    Update source code to 1.3
    Refactor code
    Show asterisk when entering password
    Add blocklist option - not working

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+using System;
+using System.IO;
 using WatiN.Core;
-using System.Windows.Forms;
+using WatiN.Core.Native.Windows;
 
 namespace K9AutoEnter
 {
@@ -13,44 +11,122 @@ namespace K9AutoEnter
         [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Please ensure all K9 windows are closed.");
+
+            Console.WriteLine("K9AutoEnter");
             Console.WriteLine("");
             Console.WriteLine("Please enter the K9 password:");
-            string k9password = Console.ReadLine();
+            string k9password = ReadPassword();
 
-            
-            using (var browser = new IE("http://127.0.0.1:2372/overrides"))
+            using (WebBrowser._browser)
             {
                 try
                 {
-                    browser.BringToFront();
+                    WebBrowser._browser.AutoClose = false;
 
-                    browser.TextField(Find.ById("b-loginContain")).TypeText(k9password);
-                    browser.Element(Find.ById("o-loginContain")).Click();
+                    //login to K9
+                    WebBrowser._browser.TextField(Find.ById("b-loginContain")).TypeText(k9password);
+                    WebBrowser._browser.Element(Find.ById("o-loginContain")).Click();
 
-                    string path_here = AppDomain.CurrentDomain.BaseDirectory;
-                    string txt_name = "K9Exceptions.txt";
-                    string[] lines = System.IO.File.ReadAllLines(path_here + txt_name);
-                    foreach (string line in lines)
-                    {
-                        browser.TextField(Find.ById("listTb-1")).TypeText(line);
-                        System.Threading.Thread.Sleep(10);
-                        System.Windows.Forms.SendKeys.SendWait("{ENTER}");
-                        browser.Element(Find.ById("listAdd-1")).Click();
-                    }
+                    WebBrowser._browser.BringToFront();
 
-                    browser.Element(Find.ById("y-saveChanges-0")).Click();
+                    //K9AllowList
+                    AddExceptions("Allow", "listTb-1", "listAdd-1");
 
-                    Console.WriteLine("Exceptions have been entered.");
-                    Console.WriteLine("Press the enter key to exit the application.");
-                    Console.ReadLine();
+                    //K9BlockList
+                    AddExceptions("Block", "listTb-0", "listAdd-0");
                 }
                 catch
                 {
-                    Console.WriteLine("There was an error entering the exceptions.");
+                    Console.WriteLine("Somthing went wrong!");
                 }
-
             }
+
+            //close up
+            Console.WriteLine("Press the enter key to exit the application.");
+            Console.ReadLine();
+        }
+
+
+        public static class WebBrowser
+        {
+            public static IE _browser;
+
+            static WebBrowser()
+            {
+                _browser = new IE("http://127.0.0.1:2372/overrides");
+            }
+
+            public static IE Current
+            {
+                get { return _browser; }
+                set { _browser = value; }
+            }
+        }
+
+        public static void AddExceptions(string exceptions_type, string exceptions_txtfld, string exceptions_addbtn)
+            {
+                using (WebBrowser._browser)
+                {
+                    try
+                    {
+                        //WebBrowser._browser.GoTo("http://127.0.0.1:2372/overrides");
+
+                        string local_dir_path = AppDomain.CurrentDomain.BaseDirectory;
+                        string full_file_name = local_dir_path + "K9" + exceptions_type + "List.txt";
+                        if (!File.Exists(full_file_name)) { return; }
+                        string[] lines = System.IO.File.ReadAllLines(full_file_name);
+                        foreach (string line in lines)
+                        {
+                            WebBrowser._browser.TextField(Find.ById(exceptions_txtfld)).TypeText(line);
+                            System.Threading.Thread.Sleep(5);
+                            System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+                            WebBrowser._browser.Element(Find.ById(exceptions_addbtn)).Click();
+                            System.Threading.Thread.Sleep(5);
+                            WebBrowser._browser.Element(Find.ById(exceptions_addbtn)).Click();
+
+                        }
+                        WebBrowser._browser.Element(Find.ById("y-saveChanges-0")).Click();
+                        Console.WriteLine(exceptions_type + " exceptions have been entered.");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("There was an error entering the " + exceptions_type + " exceptions.");
+                    }
+            }
+        }
+
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        // remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return password;
         }
     }
 }
